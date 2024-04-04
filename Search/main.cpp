cf7ab710050f06198841330024b663a877d04c13
Devya_yi23@top-academy.ru <Devya_yi23@top-academy.ru>
2024-04-04 21:42:58 +0500

    Search DONE

--- Search/main.cpp
+++ Search/main.cpp
@@ -1,19 +1,24 @@
 ﻿#include <iostream>
 using namespace std;
+
 using std::cout;
 using std::cin;
 using std::endl;
+
+//#define SEARCH_1
+#define SEARCH_2
 
 void main()
 {
     setlocale(LC_ALL, "");
 
+#ifdef SEARCH_1
     const int n = 10;
     int arr[n];
     //int minRand = 0, maxRand = 7;
     for (int i = 0; i < n; i++)
     {
-        arr[i] = rand() % 8; 
+        arr[i] = rand() % 8;
     }
     cout << "Случайные числа: " << "\t";
     for (int i = 0; i < n; i++)
@@ -21,7 +26,7 @@ void main()
         cout << arr[i] << "\t";
     }
     cout << endl;
-     int counting[8] = { 0 };
+    int counting[8] = { 0 };
     for (int i = 0; i < n; i++)
     {
         counting[arr[i]]++;
@@ -34,4 +39,43 @@ void main()
             cout << "Значение: " << i << " повторяется: " << counting[i] << " раз" << endl;
         }
     }
+#endif // SEARCH_1
+
+#ifdef SEARCH_2
+
+    const int n = 10;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = rand() % 3;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << "\t";
+    }
+    cout << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        bool met_before = false;
+        for (int j = 0; j < i; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                met_before = true;
+                break;          // прерывает текущую итерацию и все последующие
+           }
+        }
+        if (met_before)continue;// прерывает текущую итерацию и переходть к следующей
+
+        int counter = 0;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] == arr[j]) counter++;
+        }
+        if (counter) printf("Значение %i повторяется %i раз\n", arr[i], counter);
+        //if (counter)cout << " Значения " << arr[i] << " Повторяется " << counter << " Раз" << endl;
+    }
+#endif // SEARCH_2
+
 }
