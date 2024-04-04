1fa7c28b9289ac08ef43d39a0fa79006f3f9f38f
Devya_yi23@top-academy.ru <Devya_yi23@top-academy.ru>
2024-04-04 08:44:00 +0500

    Search DONE

---
+++ Search/main.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
+
+void main()
+{
+    setlocale(LC_ALL, "");
+
+    const int n = 10;
+    int arr[n];
+    //int minRand = 0, maxRand = 7;
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = rand() % 8; 
+    }
+    cout << "Случайные числа: " << "\t";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << "\t";
+    }
+    cout << endl;
+     int counting[8] = { 0 };
+    for (int i = 0; i < n; i++)
+    {
+        counting[arr[i]]++;
+    }
+    cout << "Повторяющиеся значения и количество повторений: " << endl;
+    for (int i = 0; i < 8; i++)
+    {
+        if (counting[i] > 1)
+        {
+            cout << "Значение: " << i << " повторяется: " << counting[i] << " раз" << endl;
+        }
+    }
+}
